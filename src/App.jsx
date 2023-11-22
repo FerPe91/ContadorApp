aed1be8a5fbbc9d48e5d5449e266adbdba91643b
Fernando <fernando.pereyra764@gmail.com>
2023-11-21 23:02:52 -0300

    arreglos menores

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Box, Heading, Image, Flex } from '@chakra-ui/react';
 import ContadorResponsive from './Components/ContadorResponsive/ContadorResponsive'
+import logo from '/src/assets/img/logo.png'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const imagen = logo
   return (
     <ChakraProvider>
       <Box
@@ -24,7 +25,7 @@ function App() {
         flexDirection='column'
         justifyContent='center'
         alignItems='center'>
-          <Image src={'/src/assets/img/logo.png'} alt='logo AP' />
+          <Image src={logo} alt='logo AP' />
           <Heading mt='5'>CONTADOR</Heading>
         </Flex>
         <ContadorResponsive/>
